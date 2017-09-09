1a2e205e24703e22c33f5b8c0965e3135e6ac9aa
Matthias Laug <mlaug@thoughtworks.com>
2017-09-09 14:44:30 +0200

    Pac contribution

--- pac-contribution.js
+++ pac-contribution.js
@@ -1,31 +1,10 @@
 const tabletojson = require('tabletojson');
 
 const promises = []
-const cols = [
-  ["Accounting", "IS_ACC", "RE_ACC"],
-  ["Advertising", "IS_ADV", "RE_ADV"],
-  ["Alcohol & Drugs", "IS_ALC", "RE_ALC"],
-  ["Animals", "IS_ANI", "RE_ANI"],
-  ["Budget & Appr.", "IS_BUD", "RE_BUD"],
-  ["Civil Rights & Lib.", "IS_CIV", "RE_CIV"],
-  ["Clean air & water", "IS_CAW", "RE_CAW"],
-  ["Consum prod. Safety", "IS_CSP", "RE_CSP"],
-  ["Copyright, Patent & Trademark", "IS_CPT", "RE_CPT"],
-  ["Environment & Superfund", "IS_ENV", "RE_ENV"],
-  ["Food Industry", "IS_FOO", "RE_FOO"],
-  ["Indian/native am.", "IS_IND", "RE_IND"],
-  ["Labor issues", "IS_LBR", "RE_LBR"],
-  ["Natural resources", "IS_NAT", "RE_NAT"],
-  ["Retirment", "IS_RET", "RE_RET"],
-  ["Taxes", "IS_TAX", "RE_TAX"],
-  ["Unemployment", "IS_UNM", "RE_UNM"],
-  ["Waste", "IS_WAS", "RE_WAS"],
-  ["Welfare", "IS_WEL", "RE_WEL"],
-  ["Health issues", "IS_HEA", "RE_HEA"]
-]
+
 const output = {}
-const ids = ["D000000128"]
-const years = ['2012','2013','2014','2015']
+const ids = ["C00197749"]
+const years = ['2012','2014']
 
 const add = (id, year, col, data) => {
   output[id] = output[id] || {}
@@ -34,18 +13,12 @@ const add = (id, year, col, data) => {
 
 ids.map((id) => {
   years.map((year) => {
-    promises.push(tabletojson.convertUrl(`https://www.opensecrets.org/lobby/clientissues.php?id=${id}&year=${year}`)
-      .then((data) => {
-      data[0].map((issrep) => {
-        const col = cols.find(col => col[0] === issrep.Issue);
-        if ( col ) {
-          add(id, year, col[1], issrep['Specific Issues'])
-          add(id, year, col[2], issrep['No. of Reports*'])
-        }
-        else {
-          console.log("NOT FOUND: ", issrep.Issue);
-        }
-      });
+    promises.push(tabletojson.convertUrl(`https://www.opensecrets.org/pacs/lookup2.php?strID=${id}&cycle=${year}`)
+      .then((tables) => {
+        const distribution = tables[1][0][0].match(/(\((\d+)% to Democrats, (\d+)% to Republicans\))/);
+        add(id, year, 'PACTOT', tables[1][0][1])
+        add(id, year, 'PAC_DEM', distribution[2])
+        add(id, year, 'PAC_REP', distribution[3])
     }));
   });
 });
